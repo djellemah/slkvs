874d06ce2f307e609fdb51fd1fcdbb8bc84169c8
John Anderson <panic@semiosix.com>
2024-05-08 12:58:29 -0400

    it lives!

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+mod bindings;
+
+use crate::bindings::exports::golem::component::api::*;
+use std::cell::RefCell;
+
+/// This is one of any number of data types that our application
+/// uses. Golem will take care to persist all application state,
+/// whether that state is local to a function being executed or
+/// global across the entire program.
+struct State {
+    total: u64,
+}
+
+thread_local! {
+    /// This holds the state of our application.
+    static STATE: RefCell<State> = RefCell::new(State {
+        total: 0,
+    });
+}
+
+struct Component;
+
+impl Guest for Component {
+    fn add(value: u64) {
+        STATE.with_borrow_mut(|state| state.total += value);
+    }
+
+    fn get() -> u64 {
+        STATE.with_borrow(|state| state.total)
+    }
+}
