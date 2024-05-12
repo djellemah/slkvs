e126b595892eaa7548ed662bf3c8d9cfcfc27bf2
John Anderson <panic@semiosix.com>
2024-05-12 15:02:16 -0400

    make ergonomics of path manipulation a bit nicer

--- src/tree.rs
+++ src/tree.rs
@@ -1,17 +1,10 @@
 #[allow(unused_imports)]
 use std::cell::RefCell;
 
-/// Allow for pre-hashing this, since it will be used in many comparisons
 #[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub enum Step {
   Key(String),
   Index(usize),
-}
-
-impl<'a> AsRef<Step> for Step {
-  fn as_ref(&self) -> &Self {
-    &self
-  }
 }
 
 impl std::fmt::Display for &Step {
@@ -26,6 +19,12 @@ impl std::fmt::Display for &Step {
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub struct SchemaPath(Vec<Step>);
 
+impl SchemaPath {
+  pub fn singleton(step : Step) -> Self {
+    Self(vec![step])
+  }
+}
+
 impl std::fmt::Display for SchemaPath {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     let string_parts = self.0
@@ -33,7 +32,7 @@ impl std::fmt::Display for SchemaPath {
       .map(|step| step.to_string())
       .collect::<Vec<String>>();
 
-    write!(f, "{}", string_parts.join("/"))
+    f.write_str(&string_parts.join("/"))
   }
 }
 
@@ -47,17 +46,111 @@ impl From<String> for SchemaPath {
   }
 }
 
-#[allow(dead_code)]
+impl From<Vec<Step>> for SchemaPath {
+  fn from(steps : Vec<Step>) -> Self {
+    Self(steps)
+  }
+}
+
+use std::ops::Add;
+
+impl Add<Step> for SchemaPath
+{
+  type Output = Self;
+  fn add(self, single : Step) -> <Self as Add<SchemaPath>>::Output {
+    let mut rv : Vec<Step> = vec![];
+    rv.extend_from_slice(self.0.as_slice());
+    rv.push(single);
+    Self(rv)
+  }
+}
+
+impl Add for SchemaPath {
+  type Output = Self;
+  fn add(self, rhs : SchemaPath) -> <Self as Add<SchemaPath>>::Output {
+    let mut rv : Vec<Step> = vec![];
+    match (&self,&rhs) {
+      (Self(ref lhs), Self(ref rhs)) => {
+        rv.extend_from_slice(lhs);
+        rv.extend_from_slice(rhs);
+      },
+    };
+    Self(rv)
+  }
+}
+
+impl Add<&SchemaPath> for &SchemaPath {
+  type Output = SchemaPath;
+  fn add(self, rhs : &SchemaPath) -> <Self as Add<&SchemaPath>>::Output {
+    let mut rv : Vec<Step> = vec![];
+    match (self,rhs) {
+      (SchemaPath(lhs), SchemaPath(rhs)) => {
+        rv.extend_from_slice(lhs);
+        rv.extend_from_slice(rhs);
+      },
+    };
+    SchemaPath(rv)
+  }
+}
+
+impl Add<&Vec<Step>> for SchemaPath
+{
+  type Output = Self;
+  fn add(self, rhs : &Vec<Step>) -> <Self as Add<SchemaPath>>::Output {
+    let mut rv : Vec<Step> = vec![];
+    rv.extend_from_slice(self.0.as_slice());
+    rv.extend_from_slice(&rhs);
+    Self(rv)
+  }
+}
+
+impl Add<Step> for &SchemaPath {
+  type Output = SchemaPath;
+  fn add(self, single : Step) -> <Self as Add<Step>>::Output {
+    let mut rv : Vec<Step> = vec![];
+    rv.extend_from_slice(self.0.as_slice());
+    rv.push(single);
+    SchemaPath(rv)
+  }
+}
+
+// pub trait LeafStorage = AsRef<str>; also works
+pub trait LeafStorage = AsRef<[u8]>;
+
+#[allow(dead_code)] // because Number and Boolean aren't currently used
 #[derive(Eq, PartialEq, Debug, Clone, Hash, Copy)]
 pub enum Leaf<T>
-where
-T: AsRef<[u8]>, // because we want storage
+where T : LeafStorage, // because we want storage
 {
   String(T),
   // Leave it as string representation for now
-  Number(T),
+  Number(i64),
   Boolean(bool),
   Null,
+}
+
+// E0119 :-(
+
+// impl<T: LeafStorage> From<i64> for Leaf<T> {
+//   fn from(src: i64) -> Self {
+//     Self::Number(src)
+//   }
+// }
+
+// impl<T: std::convert::AsRef<[u8]>> From<bool> for Leaf<T> {
+//   fn from(src: bool) -> Self {
+//     Self::Boolean(src)
+//   }
+// }
+
+impl<T, Src> From<Src> for Leaf<T>
+where
+  T: LeafStorage + std::convert::From<Src>,
+  Src : std::convert::AsRef<str>,
+{
+  fn from(src: Src) -> Self {
+    Self::String(src.into())
+  }
 }
 
 // type PathMap<K,V> = std::collections::HashMap<K, V>;
@@ -67,8 +160,6 @@ pub struct LeafPaths(pub PathMap<SchemaPath,Leaf<String>>);
 // 'Ding' cos that's what happens when you get an error.
 #[derive(Debug)]
 pub struct DingString(String);
-
-impl std::error::Error for DingString {}
 
 impl std::fmt::Display for DingString {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
@@ -99,31 +190,31 @@ impl LeafPaths {
     self.0.keys().map(ToString::to_string).collect()
   }
 
-  pub fn add(&mut self, path: String, leaf: String) {
-    self.0.insert(path.into(), Leaf::String(leaf));
+  fn insert(&mut self, path : SchemaPath, leaf : Leaf<String>) -> Option<Leaf<String>> {
+    self.0.insert(path, leaf)
   }
 
-  fn add_at_path(&mut self, path: SchemaPath, json_obj: serde_json::Value) {
+  pub fn add(&mut self, path: String, leaf: String) {
+    self.insert(path.into(), Leaf::String(leaf));
+  }
+
+  fn add_at_path(&mut self, base_path : SchemaPath, json_obj: serde_json::Value) {
     use serde_json::Value::*;
-    // These all return Option<_> but we don't care
-    let _ = match json_obj {
-      Null => self.0.insert(path, Leaf::String("null".into())),
-      Bool(v) => self.0.insert(path, Leaf::String(format!("{v}"))),
-      Number(v) => self.0.insert(path, Leaf::String(format!("{v}"))),
-      String(v) => self.0.insert(path, Leaf::String(v)),
+    // These all return Option<_> with the previous value but we don't care
+    match json_obj {
+      Null => self.insert(base_path, "null".into()),
+      Bool(v) => self.insert(base_path, format!("{v}").into()),
+      Number(v) => self.insert(base_path, format!("{v}").into()),
+      String(v) => self.insert(base_path, v.into()),
       Array(ary) => {
         for (i, obj) in ary.into_iter().enumerate() {
-          let mut path = path.0.clone();
-          path.push(Step::Index(i));
-          self.add_at_path(SchemaPath(path), obj);
+          self.add_at_path(&base_path + Step::Index(i), obj);
         }
         None
       }
       Object(obj) => {
         for (key, val) in obj {
-          let mut path = path.0.clone();
-          path.push(Step::Key(key));
-          self.add_at_path(SchemaPath(path), val);
+          self.add_at_path(&base_path + Step::Key(key), val);
         }
         None
       }
@@ -131,9 +222,8 @@ impl LeafPaths {
   }
 
   pub fn addtree(&mut self, path: String, json: String) -> Result<(), DingString> {
-    let path: SchemaPath = path.into();
     let json = serde_json::from_str(json.as_str())?;
-    self.add_at_path(path, json);
+    self.add_at_path(path.into(), json);
     Ok(())
   }
 
@@ -145,14 +235,15 @@ impl LeafPaths {
 
 impl<T> std::fmt::Display for Leaf<T>
 where
-  T: std::convert::AsRef<[u8]> + std::fmt::Display
+  T: LeafStorage + std::fmt::Display
 {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+    use Leaf::*;
     match &self {
-      Leaf::String(str) => write!(f, "{}", str),
-      Leaf::Number(_) => todo!(),
-      Leaf::Boolean(_) => todo!(),
-      Leaf::Null => todo!(),
+      String(v) => write!(f, "{}", v),
+      Number(v) => write!(f, "{}", v),
+      Boolean(v) => write!(f, "{}", v),
+      Null => write!(f,"null"),
     }
   }
 }
@@ -183,16 +274,16 @@ mod t {
   fn listpaths() {
     let path = path_of_strs!["uno", "due", "tre"];
     let mut leaf_paths = LeafPaths::new();
-    leaf_paths.0
-      .insert(path, Leaf::String("empty not empty".into()));
+    leaf_paths.0.insert(path, Leaf::String("empty not empty".into()));
+
     assert_eq!(leaf_paths.listpaths(), vec!["uno/due/tre"]);
+    assert_eq!(leaf_paths.get("uno/due/tre".into()),Some("empty not empty".into()));
   }
 
   #[test]
   fn get() {
     let mut leaf_paths = LeafPaths::new();
-    leaf_paths.0
-      .insert(path_of_strs!["wut"], Leaf::String("empty not empty".into()));
+    leaf_paths.0 .insert(path_of_strs!["wut"], Leaf::String("empty not empty".into()));
 
     assert_eq!(leaf_paths.listpaths(), vec!["wut"]);
     assert_eq!(leaf_paths.get("wut".into()), Some("empty not empty".into()));
@@ -207,7 +298,7 @@ mod t {
 
     let mut hshr = std::hash::DefaultHasher::new();
     let leaf_string = Leaf::String("hello");
-    let leaf_number = Leaf::Number("5");
+    let leaf_number : Leaf<String> = Leaf::Number(5);
     leaf_string.hash(&mut hshr);
     leaf_number.hash(&mut hshr);
     assert_eq!(hshr.finish(), 3319468350668666690);
@@ -259,14 +350,8 @@ mod t {
     let expected_value_one = Leaf::String("some value".to_string());
     let expected_value_two = Leaf::String("this".to_string());
 
-    assert_eq!(
-      leaf_paths.0.get(&expected_path_one).unwrap(),
-      &expected_value_one
-      );
-    assert_eq!(
-      leaf_paths.0.get(&expected_path_two).unwrap(),
-      &expected_value_two
-      );
+    assert_eq!( leaf_paths.0.get(&expected_path_one).unwrap(), &expected_value_one );
+    assert_eq!( leaf_paths.0.get(&expected_path_two).unwrap(), &expected_value_two );
   }
 
   #[test]
@@ -294,9 +379,7 @@ mod t {
     }"#;
 
     let mut leaf_paths = LeafPaths::new();
-    leaf_paths
-    .addtree("uno/due/tre".into(), json.into())
-    .unwrap();
+    leaf_paths.addtree("uno/due/tre".into(), json.into()).unwrap();
 
     let expected_path_one = path_of_strs!["uno", "due", "tre", "next", "inner"];
     let expected_value_one = Leaf::String("some value".to_string());
@@ -312,9 +395,7 @@ mod t {
     let json = r#""singular": "bad bad json"#;
 
     let mut leaf_paths = LeafPaths::new();
-    let err = leaf_paths
-      .addtree("uno/due/tre".into(), json.into())
-      .unwrap_err();
+    let err = leaf_paths.addtree("uno/due/tre".into(), json.into()).unwrap_err();
 
     assert_eq!( err.to_string(), "Error(\"trailing characters\", line: 1, column: 11)" );
   }
